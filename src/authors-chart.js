f1d28d7a96a4b569a72838b7c65a2ad3d8ac73a2
tell-y <t.yamamoto28@gmail.com>
2018-04-25 00:36:15 +0900

    set colors

--- src/authors-chart.js
+++ src/authors-chart.js
@@ -1,18 +1,19 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
 import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+import randomcolor from 'randomcolor';
 import fs from 'fs';
 import dataLoader from './data-loader';
 
 const main = async () => {
   const fetched = await dataLoader();
-
+  const colors = randomcolor({ count: fetched.authors.length });
   const AuthorsChart = () => (
     <LineChart width={1000} height={600} data={fetched.data}>
       <XAxis dataKey="date" />
       <YAxis />
       <CartesianGrid stroke="#eee" />
-      {fetched.authors.map(author => <Line type="monotone" dataKey={author} />)}
+      {fetched.authors.map((author, i) => <Line type="monotone" dataKey={author} stroke={colors[i]} />)}
     </LineChart>
   );
 

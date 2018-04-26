308279423f1c6d540c8dc2df48ead04c0535bed0
tell-y <t.yamamoto28@gmail.com>
2018-04-26 20:56:56 +0900

    output as html instead of svg only

--- src/authors-chart.js
+++ src/authors-chart.js
@@ -1,26 +1,51 @@
 import React from 'react';
 import ReactDOMServer from 'react-dom/server';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+import { LineChart, Line, XAxis, YAxis, CartesianGrid, Legend, Label } from 'recharts';
 import randomcolor from 'randomcolor';
 import fs from 'fs';
 import dataLoader from './data-loader';
 
 const main = async () => {
   const fetched = await dataLoader();
-  const colors = randomcolor({ count: fetched.authors.length });
+  const colors = fetched.authors.map(author => randomcolor({ count: 1, seed: author })[0]);
+  const latest = fetched.data[fetched.data.length - 1];
   const AuthorsChart = () => (
-    <LineChart width={1000} height={600} data={fetched.data}>
+    <LineChart
+      width={1000}
+      height={600}
+      data={fetched.data}
+      margin={{ top: 30, right: 30, left: 30, bottom: 30 }}
+    >
       <XAxis dataKey="date" />
-      <YAxis />
+      <YAxis>
+        <Label value="アプリ数" offset={15} position="top" />
+      </YAxis>
       <CartesianGrid stroke="#eee" />
-      {fetched.authors.map((author, i) => <Line type="monotone" dataKey={author} stroke={colors[i]} />)}
+      <Legend
+        wrapperStyle={{ position: 'relative' }}
+        content={({ payload }) => (
+          <ul>
+            {payload
+              .filter(e => latest[e.value] > 5)
+              .sort((a, b) => latest[b.value] - latest[a.value])
+              .map((e, index) => (
+                <li key={`item-${index}`} style={{ color: e.color }}>
+                  {e.value}({latest[e.value]})
+                </li>
+              ))}
+            <li style={{ color: 'gray' }}>and more...</li>
+          </ul>
+        )}
+      />
+      {fetched.authors.map((author, i) => (
+        <Line name={author} type="monotone" dataKey={author} stroke={colors[i]} />
+      ))}
     </LineChart>
   );
 
   const rendered = ReactDOMServer.renderToString(<AuthorsChart />);
-  const chart = rendered.match(/<svg.+<\/svg>/)[0];
-  const formatted = chart.replace('class="recharts-surface"', 'xmlns="http://www.w3.org/2000/svg"');
-  fs.writeFileSync('./outputs/authors-chart.svg', withXmlns);
+  const html = content => `<html><head><meta charset="utf-8"></head><body>${content}</body></html>`;
+  fs.writeFileSync('./outputs/authors-chart.html', html(rendered));
 };
 
 main();
